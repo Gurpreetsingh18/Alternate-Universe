6d09254c020eaea7935b7be794e68a395f879efb
Gurpreetsingh18 <61836288+Gurpreetsingh18@users.noreply.github.com>
2020-06-01 09:26:22 +0000

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -2,58 +2,63 @@
 var earth;
 var sun;
 var mercury;
-var jupiter;
-var neptune;
-var mars;
-var saturn;
-var uranus;
 var venus;
-var world;
+var image1,image2,image3,image4;
+var frame = [sun];
 
- 
+
+
+ function preload(){
+  image1 = loadImage("sun.png");
+  image2 = loadImage("mercury.png");
+  image3 = loadImage("venus.png");
+  image4 = loadImage("earth.png");
+ }
+
+
+
 function setup() {
-    createCanvas(1200, 1200);
-    
-
-    sun = createSprite(150,300,50,50);
-    mercury = createSprite(250,300,50,50);
-    venus = createSprite(350,300,50,50);
-
+    createCanvas(600,600);
+    sun = createSprite(300,300,50,50);
+    sun.addImage("sun",image1);
+    sun.scale =0.3;
+    mercury = createSprite(150,380,50,50);
+    mercury.addImage("mercury",image2);
+    mercury.scale = 0.1;
+    venus = createSprite(350,100,50,50);
+    venus.addImage("venus",image3);
+    venus.scale = 0.1;
     earth = createSprite(450,300,50,50);
-    mars = createSprite(550,300,50,50);
-    jupiter = createSprite(650,300,50,50);
-
-    saturn = createSprite(750,300,50,50);
-    uranus = createSprite(850,300,50,50);
-    neptune = createSprite(950,300,50,50);
-
+    earth.addImage("earth",image4);
+    earth.scale = 0.1;
+    
 }
 
 function draw() {
   background(0);
   strokeWeight(4);
+  sun.display();
+  earth.display();
+  venus.display();
+  mercury.display();
+  text("Press mouse Over Sun",200,400);
+  stroke(255);
 
 
-  text("Sun",130,260);
-  textSize(10);
-  text("Mercury",230,260);
-  textSize(10);
-  text("Venus",330,260);
-  textSize(10);
-  text("Earth",440,260);
-  textSize(10);
-  text("Mars",540,260);
-  textSize(10);
-  text("Jupiter",630,260);
-  textSize(10);
-  text("Saturn",730,260);
-  textSize(10);
-  text("Uranus",830,260);
-  textSize(10);
-  text("Neptune",930,260);
-  textSize(10);
- 
+  if(sun.isTouching(mercury)){
+    mercury.destroy();
+  }
+  if(sun.isTouching(venus)){
+    venus.destroy();
+  }
+  if(sun.isTouching(earth)){
+    earth.destroy();
+  }
 
-  
-  drawSprites();
+  if(mousePressedOver(sun)){
+    sun.scale++
+  }
+
 }
+
+
